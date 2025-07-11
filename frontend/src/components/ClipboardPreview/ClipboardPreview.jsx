72fe143cc2fdfb7b15faf95db6b220e4e1dba223
rio.sung.ks@gmail.com <rio.sung.ks@gmail.com>
2025-07-11 16:39:54 +0900

    Feat: 파일 업로드 확인 및 삭제

--- frontend/src/components/ClipboardPreview/ClipboardPreview.jsx
+++ frontend/src/components/ClipboardPreview/ClipboardPreview.jsx
@@ -3,13 +3,10 @@ import './ClipboardPreview.css';
 import { Button } from './Button/Button';
 import { Textarea } from './Button/Textarea';
 
-export const ClipboardPreview = ({
-  analyzeClipboard,
-  onSubmit,
-  onGetQuizzes,
-  isPendingQuestion,
-}) => {
+export const ClipboardPreview = ({ analyzeClipboard, onSubmit }) => {
   const [preview, setPreview] = useState(null);
+  const [fileName, setFileName] = useState(null);
+  const [File, setFile] = useState(null);
   const inputRef = useRef(null);
   const myClipBoard = navigator.clipboard;
 
@@ -27,19 +24,38 @@ export const ClipboardPreview = ({
   const handlePDFupload = async () => {
     inputRef.current.click();
   };
+  const handleFileChange = e => {
+    const file = e.target.files[0];
+    if (file) {
+      setFile(file);
+      setFileName(file.name);
+      console.log(file.name);
+    }
+  };
+  const handleDeleteFile = async () => {
+    setFileName(null);
+    setFile(null);
+    if (inputRef.current) {
+      inputRef.current.value = null;
+    }
+  };
 
   return (
     <>
-      <div>
+      <div className="flex justify-between text-lg mb-4">
         <Button onClick={handlePreview} text={'클립보드 미리보기'} />
-        <Button onClick={handlePDFupload} text={'파일 업로드'} />
+        <Button onClick={handlePDFupload} text={'📁 파일 선택'} />
         <input
           type="file"
           accept="image/*"
           ref={inputRef}
-          // onChange={handleFileChange}
+          onChange={handleFileChange}
           style={{ display: 'none' }}
         />
+      </div>
+      <div className="text-right">{fileName}</div>
+      <div className="bg_hover_text" onClick={handleDeleteFile}>
+        삭제
       </div>
       <div>
         <Textarea preview={preview} />
